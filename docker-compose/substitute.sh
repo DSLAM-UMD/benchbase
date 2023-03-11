0c63d575c05dd055195458da3b78cac4d482aa4e
Cuong Nguyen <ctring23@gmail.com>
2023-03-11 18:24:49 -0500

    Upload results to minio

--- docker-compose/substitute.sh
+++ docker-compose/substitute.sh
@@ -5,8 +5,7 @@ template="/${BENCHMARK}-config-template.xml"
 out="/${BENCHMARK}-config.xml"
 
 cp $template $out
-sed -i "s/BB_HOST/${HOST}/" $out
-sed -i "s/BB_PORT/${PORT}/" $out 
+sed -i "s/BB_ADDRESS/${ADDRESS}/" $out
 sed -i "s/BB_DATABASE/${DATABASE}/" $out 
 sed -i "s/BB_USERNAME/${USERNAME}/" $out 
 sed -i "s/BB_PASSWORD/${PASSWORD}/" $out 
