d9621e25c400add15a6134c7dfa38a841b20509b
Cuong Nguyen <ctring23@gmail.com>
2023-07-01 00:32:27 -0400

    USER => DBUSER

--- docker-compose/substitute.sh
+++ docker-compose/substitute.sh
@@ -7,7 +7,7 @@ out="/${BENCHMARK}-config.xml"
 cp $template $out
 sed -i "s/BB_ADDRESS/${ADDRESS}/" $out
 sed -i "s/BB_DATABASE/${DATABASE}/" $out 
-sed -i "s/BB_USERNAME/${USER}/" $out 
+sed -i "s/BB_USERNAME/${DBUSER}/" $out 
 sed -i "s/BB_PASSWORD/${PASSWORD}/" $out 
 sed -i "s/BB_REGION/${REGION}/" $out 
 sed -i "s/BB_NUM_REGIONS/${NUM_REGIONS}/" $out
